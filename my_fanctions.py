32bc561da4b1209310f28c4f3802431de425e599
Agatadell12 <149382123+Agatadell12@users.noreply.github.com>
2024-01-13 12:20:22 +0100

    work-in-progress-workers
    Dodano funkcje wywołujące listę pracowników i poprawiono funkcje wywołujące oddziały

--- my_fanctions.py
+++ my_fanctions.py
@@ -1,8 +1,9 @@
-from psycopg2 import cursor
 import psycopg2
 from bs4 import BeautifulSoup
 import requests
 import folium
+
+#Logowanie do aplikacjii
 
 db_params = psycopg2.connect(
     database='postgres',
@@ -13,8 +14,7 @@ db_params = psycopg2.connect(
 )
 
 
-
-# Funkcje aplikacji
+#Funkcje do wyszukiwania,dodawania,aktualizowania,usuwania oddziałów oraz generowanie mapy wszystkich oddziałów
 
 def add_unit() -> None:
     """
@@ -24,11 +24,11 @@ def add_unit() -> None:
     cursor = db_params.cursor()
 
     # Pobierz dane od użytkownika
-    oddzial = input('Podaj nazwę oddziału:')
-    miejscowosc = input('Podaj nazwę miasta:')
+    nazwa = input('Podaj nazwę oddziału:')
+    city = input('Podaj nazwę miasta:')
 
     # Wykonaj zapytanie SQL, aby dodać nowy oddział
-    sql_query = f"INSERT INTO public.mudnurowki(oddzial, miejscowosc) VALUES('{oddzial}', '{miejscowosc}');"
+    sql_query = f"INSERT INTO public.oddzialy(nazwa, city) VALUES('{nazwa}', '{city}');"
     cursor.execute(sql_query)
 
     # Zatwierdź zmiany
@@ -39,134 +39,297 @@ def add_unit() -> None:
 
 
 
+def show_unit(db_params) -> None:
+    # Utwórz kursor w oparciu o db_params
+    cursor = db_params.cursor()
+
+    sql_query_1 = f"SELECT * FROM public.oddzialy;"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    for row in query_result:
+        print(f'Oddział {row[1]} w miejscowości {row[2]}')
+
+    # Zamknij kursor po użyciu
+    cursor.close()
+
+
+
+
+def remove_unit(db_params) -> None:
+    """
+    remove custom object from list
+    :param users_list: list - user list
+    :return: None
+    """
+    cursor = db_params.cursor()
+    oddzialy = input('Podaj oddzial do usuniecia:')
+    sql_query_1 = f"SELECT * FROM public.oddzialy WHERE nazwa='{oddzialy}';"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    print(f'znaleziono ')
+    print('0: Usuń wszystkie znalezione oddziały')
+
+    for numerek, user_to_be_removed in enumerate(query_result):
+        print(f'{numerek + 1}. {user_to_be_removed}')
+
+    numer = int(input(f'Podaj nazwę oddziału do usunięcia '))
+
+    if numer == 0:
+        sql_query_2 = f"DELETE FROM public.oddzialy;"
+        cursor.execute(sql_query_2)
+        db_params.commit()
+    elif 0 < numer <= len(query_result):
+        user_id_to_remove = query_result[numer - 1][0]
+        sql_query_2 = f"DELETE FROM public.oddzialy WHERE id='{query_result[numer - 1][0]}';"
+        cursor.execute(sql_query_2)
+        db_params.commit()
+    else:
+        print('Błędny numer, nie usunięto żadnego użytkownika.')
+
+        cursor.close()
+
+def update_unit(db_params) -> None:
+    cursor = db_params.cursor()
+    unit = input("Podaj oddział do modyfikacji:")
+    sql_query_1 = f"SELECT * FROM public.oddzialy WHERE nazwa ='{unit}';"
+    cursor.execute(sql_query_1)
+    print('Znaleziono')
+    nazwa= input('Podaj nowy oddział: ')
+    city = input('Podaj nazwe miasta: ')
+    sql_query_2 = f"UPDATE public.oddzialy SET nazwa='{nazwa}',city='{city}' WHERE nazwa='{unit}';"
+    cursor.execute(sql_query_2)
+    db_params.commit()
+
+    cursor.close()
+
+
+def get_coordinate_of(city: str) -> list[float, float]:
+    # Pobranie strony internetowej
+
+    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
+    response = requests.get(url=adres_URL)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+
+    # Pobranie współrzędnych z treści strony internetowej
+    latitude_elements = response_html.select('.latitude')
+    longitude_elements = response_html.select('.longitude')
+
+    if len(latitude_elements) > 1 and len(longitude_elements) > 1:
+        try:
+            response_html_latitude = float(latitude_elements[1].text.replace(',', '.'))
+            response_html_longitude = float(longitude_elements[1].text.replace(',', '.'))
+            return [response_html_latitude, response_html_longitude]
+        except ValueError as e:
+            print(f"Błąd konwersji współrzędnych: {e}")
+    else:
+        print("Błąd: Nie udało się znaleźć elementów z szerokością i długością geograficzną na stronie internetowej.")
+
+    return [0.0, 0.0]  # Domyślne współrzędne w przypadku błędu
+
+
+def get_map_of(db_params) -> None:
+    cursor = db_params.cursor()
+    map = folium.Map(
+        location=[52.3, 21.8],
+        tiles='OpenStreetMap',
+        zoom_start=14, )
+    sql_query_1 = f"SELECT * FROM public.oddzialy;"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    for user in query_result:
+        folium.Marker(
+            location=get_coordinate_of(city=user[2]),
+            popup=f'Oddział: {user[1]}\n'
+                  f'{user[2]}'
+        ).add_to(map)
+        map.save(f'mapka.html')
+
+    cursor.close()
+
+#Funkcje do wyszukiwania,dodawania,aktualizowania,usuwania pracowników ze wszytkich oddziałów oraz generowanie mapy wszystkich pracowników
+
+def add_workers() -> None:
+    """
+    Dodaj oddział do listy.
+    """
+    # Utwórz kursor w oparciu o db_params (uzyskasz dostęp do zmiennej globalnej)
+    cursor = db_params.cursor()
+
+    # Pobierz dane od użytkownika
+    imie = input('Podaj imię prawcownika:')
+    nazwisko = input('Podaj nazwisko pracownika:')
+    city = input('Podaj nazwę miasta:')
+    stanowiskopracy = input('Podaj stanowisko pracy pracownika:')
+    oddzial = input('Podaj oddzial w którym pracuje pracownik:')
+
+    # Wykonaj zapytanie SQL, aby dodać nowy oddział
+    sql_query = f"INSERT INTO public.pracownicy(imie, nazwisko, city, stanowiskopracy, oddzial) VALUES('{imie}', '{nazwisko}', '{city}','{stanowiskopracy}','{oddzial}');"
+    cursor.execute(sql_query)
+
+    # Zatwierdź zmiany
+    db_params.commit()
+
+    # Zamknij kursor po użyciu
+    cursor.close()
+
+
+
+def show_workers(db_params) -> None:
+    # Utwórz kursor w oparciu o db_params
+    cursor = db_params.cursor()
+
+    sql_query_1 = f"SELECT * FROM public.pracownicy;"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    for row in query_result:
+        print(f'{row[1]} {row[2]} z miejscowości {row[3]} pracujący na stanowisku {row[4]} w oddziale {row[5]}')
+
+    # Zamknij kursor po użyciu
+    cursor.close()
+
+
+
+
+def remove_workers(db_params) -> None:
+    cursor = db_params.cursor()
+    nazwisko = input('Podaj nazwisko pracownika do usunięcia:')
+    sql_query_1 = f"SELECT * FROM public.pracownicy WHERE nazwisko='{nazwisko}';"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    print(f'Znaleziono:')
+    print('0: Usuń wszystkich znalezionych pracowników')
+
+    for numerek, user_to_be_removed in enumerate(query_result):
+        print(f'{numerek + 1}. {user_to_be_removed}')
+
+    numer = int(input(f'Podaj numer pracownika do usunięcia '))
+
+    if numer == 0:
+        sql_query_2 = f"DELETE FROM public.pracownicy;"
+        cursor.execute(sql_query_2)
+        db_params.commit()
+    elif 0 < numer <= len(query_result):
+        user_id_to_remove = query_result[numer - 1][0]
+        sql_query_2 = f"DELETE FROM public.pracownicy WHERE id='{query_result[numer - 1][0]}';"
+        cursor.execute(sql_query_2)
+        db_params.commit()
+        print(f'Usunięto pracownika o nazwisku {nazwisko}.')
+    else:
+        print('Błędny numer, nie usunięto żadnego pracownika.')
+
+    cursor.close()
+
+
+def update_workers(db_params) -> None:
+    cursor = db_params.cursor()
+    unit = input("Podaj nazwisko pracownika do modyfikacji:")
+    sql_query_1 = f"SELECT * FROM public.pracownicy WHERE nazwisko ='{unit}';"
+    cursor.execute(sql_query_1)
+    print('Znaleziono')
+    imie= input('Podaj imię pracownika:')
+    nazwisko= input('Podaj nazwisko pracownika:')
+    city = input('Podaj nazwe miasta:')
+    stanowiskopracy = input('Podaj stanowisko pracy:')
+    oddzial = input('Podaj oddział w którym pracuje:')
+    sql_query_2 = f"UPDATE public.pracownicy SET imie='{imie}',nazwisko='{nazwisko}',city='{city}',stanowiskopracy='{stanowiskopracy}',oddzial='{oddzial}' WHERE nazwisko='{unit}';"
+    cursor.execute(sql_query_2)
+    db_params.commit()
+
+    cursor.close()
+
+
+def get_map_of_workers(db_params) -> None:
+    cursor = db_params.cursor()
+    map = folium.Map(
+        location=[52.3, 21.8],
+        tiles='OpenStreetMap',
+        zoom_start=14, )
+    sql_query_1 = f"SELECT * FROM public.pracownicy;"
+    cursor.execute(sql_query_1)
+    query_result = cursor.fetchall()
+    for user in query_result:
+        city = user[3]  # Zakładając, że informacja o miejscowości znajduje się w 4. kolumnie (indeks 3)
+        folium.Marker(
+            location=get_coordinate_of(city),
+            popup=f'Pracownik: {user[1]} {user[2]}\n'
+                  f'mieszka w {user[3]}'
+        ).add_to(map)
+    map.save(f'mapkapracownicy.html')
+
+    cursor.close()
+
+#Funkcje do wyszukiwania,dodawania,aktualizowania,usuwania żolnierzy którzy pobrali sorty mundurowe ze wszytkich oddziałów
+
+
+
 def gui(db_params) -> None:
     while True:
         print(f'MENU\n'
               f'0: Wyświetl liste oddziałów zaopatrywania w mundury\n'
-              f'1: Dodaj do listy oddziałow zaopatrywania w mundury oddział\n'
-              f'2: Usuń z listy odziałów zaopatrywania w mundury oddział\n'
-              f'3: Aktualizuj listę oddziałów zaopatrywania w mundury\n'
-              f'4: Wygeneruj mapę wszystkich oddziałów\n'
-              f'5: Wyjdź')
+              f'1: Wyświetl liste pracowników\n'
+              f'2: Wyświetl liste żołnierzy którzy pobrali sorty mundurowe\n'
+              f'3: Dodaj do listy oddziałow zaopatrywania w mundury oddział\n'
+              f'4: Dodaj do listy pracownika\n'
+              f'5: Dodaj do listy żołnierza\n'
+              f'6: Usuń z listy odziałów zaopatrywania w mundury oddział\n'
+              f'7: Usuń z listy pracownika\n'
+              f'8: Usuń z listy żołnierza\n'
+              f'9: Aktualizuj listę oddziałów zaopatrywania w mundury\n'
+              f'10: Aktualizuj listę pracowników\n'
+              f'11: Aktualizuj listę ożołnierzy\n'
+              f'12: Wygeneruj mapę wszystkich oddziałów\n'
+              f'13: Wygeneruj mapę wszystkich pracowników\n'
+              f'14: Wyjdź')
 
         menu_option = input('Podaj funkcje do wywołania')
         print(f'Wybrane funkcje {menu_option}')
 
         match menu_option:
             case '0':
-                print('Wyświetl liste oddziałów zaopatrywania w mundury')
+                print('Oddziały zaopatrujące żołnierzy w mundury')
                 show_unit(db_params)
-            case '1':
-                print('Dodaj do listy oddziałow zaopatrywania w mundury oddział')
-                add_unit()
+            case'1':
+                print('Lista pracowników')
+                show_workers(db_params)
             case '2':
-                print('Usuń z listy odziałów zaopatrywania w mundury oddział')
-                remove_unit()
+                print('Lista żołnierzy którzy pobrali sorty mundurowe')
+                show_unit(db_params)
             case '3':
-                print('Aktualizuj listę oddziałów zaopatrywania w mundury')
-                update_unit()
+                print('Dodaj do listy nowy oddział')
+                add_unit()
             case '4':
-                print('Wygeneruj mapę wszystkich oddziałów')
-                get_map_of()
+                print('Dodaj do listy nowego pracownika')
+                add_workers()
             case '5':
+                print('Dodaj do listy nowego żołnierza')
+                add_unit()
+            case '6':
+                print('Usuń z listy oddział')
+                remove_unit(db_params)
+            case '7':
+                print('Usuń z listy pracownika')
+                remove_workers(db_params)
+            case '8':
+                print('Usuń z listy żołnierza')
+                remove_unit(db_params)
+            case '9':
+                print('Aktualizuj listę oddziałów zaopatrywania w mundury')
+                update_unit(db_params)
+            case '10':
+                print('Aktualizuj listę pracowników')
+                update_workers(db_params)
+            case '11':
+                print('Aktualizuj listę żołnierzy')
+                update_unit(db_params)
+            case '12':
+                print('Wygeneruj mapę wszystkich oddziałów')
+                get_map_of(db_params)
+            case '13':
+                print('Wygeneruj mapę wszystkich pracowników')
+                get_map_of_workers(db_params)
+            case '14':
                 print('Kończę pracę')
                 break
 
-
-def show_unit(db_params) -> None:
-    # Utwórz kursor w oparciu o db_params
-    cursor = db_params.cursor()
-
-    sql_query_1 = f"SELECT * FROM public.mudnurowki;"
-    cursor.execute(sql_query_1)
-    query_result = cursor.fetchall()
-    for row in query_result:
-        print(f'{row[2]}')
-
-    # Zamknij kursor po użyciu
-    cursor.close()
-
-
-# reszta kodu bez zmian...
-
-# Wywołanie funkcji gui() z przekazaniem db_params jako argument
 gui(db_params)
-
-
-
-def remove_unit() -> None:
-    """
-    remove custom object from list
-    :param users_list: list - user list
-    :return: None
-    """
-    oddzial = input('Podaj oddzial do usuniecia:')
-    sql_query_1 = f"SELECT * FROM public.mudnurowki WHERE oddzial='{oddzial}';"
-    cursor.execute(sql_query_1)
-    query_result = cursor.fetchall()
-    print(f'znaleziono ')
-    print('0: usun wszystkich znalezionych użytkowników')
-
-    for numerek, user_to_be_removed in enumerate(query_result):
-        print(f'{numerek + 1}. {user_to_be_removed}')
-
-    numer = int(input(f'Podaj użytkownika do usunięcia '))
-
-    if numer == 0:
-        sql_query_2 = f"DELETE FROM public.mudnurowki;"
-        cursor.execute(sql_query_2)
-        db_params.commit()
-    elif 0 < numer <= len(query_result):
-        user_id_to_remove = query_result[numer - 1][0]
-        sql_query_2 = f"DELETE FROM public.mudnurowki WHERE id='{query_result[numer - 1][0]}';"
-        cursor.execute(sql_query_2)
-        db_params.commit()
-    else:
-        print('Błędny numer, nie usunięto żadnego użytkownika.')
-
-def update_unit() -> None:
-    unit = input("Podaj oddział do modyfikacji:")
-    sql_query_1 = f"SELECT * FROM public.mudnurowki WHERE oddzial='{unit}';"
-    cursor.execute(sql_query_1)
-    print('Znaleziono')
-    oddzial = input('Podaj nowy oddział: ')
-    miejscowosc = input('Podaj nazwe miasta: ')
-    sql_query_2 = f"UPDATE public.mudnurowki SET oddzial='{oddzial}',miejscowosc='{miejscowosc}' WHERE oddzial='{unit}';"
-    cursor.execute(sql_query_2)
-    db_params.commit()
-
-def get_cooordinate_of(city: str) -> list[float, float]:
-    # pobranie strony internetowej
-
-    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
-    response = requests.get(url=adres_URL) #zwraca obiekty
-    response_html = BeautifulSoup(response.text, 'html.parser')
-
-    # pobranie współrzędnych z treści strony internetowej
-    response_html_latitude = response_html.select('.latitude')[1].text  # . ponieważ class
-    response_html_latitude = float(response_html_latitude.replace(',', '.'))
-
-    response_html_longitude = response_html.select('.longitude')[1].text  # . ponieważ class
-    response_html_longitude = float(response_html_longitude.replace(',', '.'))
-
-    return [response_html_latitude, response_html_longitude]
-
-def get_map_of() -> None:
-    map = folium.Map(
-        location=[52.3, 21.8],
-        tiles='OpenStreetMap',
-        zoom_start=14, )
-    sql_query_1 = f"SELECT * FROM public.mudnurowki;"
-    cursor.execute(sql_query_1)
-    query_result = cursor.fetchall()
-    for user in query_result:
-        folium.Marker(
-            location=get_cooordinate_of(miejscowosc=user[1]),
-            popup=f'Użytkownik: {user[2]}\n'
-                  f'Liczba postów {user[4]}'
-        ).add_to(map)
-        map.save(f'mapka.html')
-
-
-gui()
 
